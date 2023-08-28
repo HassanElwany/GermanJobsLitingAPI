772fe66eef400d1f930ec131ecef05a566c95962
HassanElwany <elwany_hassan@hotmail.com>
2023-08-28 18:47:54 +0300

    fix for deployment

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ import axios from 'axios'
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+
+app.set('view engine', 'ejs');
+
 app.get("/", (req, res) => {
     res.render("index.ejs")
 })
